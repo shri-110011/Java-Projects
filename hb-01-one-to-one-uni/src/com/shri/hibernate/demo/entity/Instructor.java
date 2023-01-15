a7a28b935e09dde2ea52b96e279055dc763d689f
Shrikant Desktop <shrikant.test.accnt@gmail.com>
2023-01-15 15:59:11 +0530

    Commit on 15 January 2023 3:59pm IST.

--- hb-01-one-to-one-uni/src/com/shri/hibernate/demo/entity/Instructor.java
+++ hb-01-one-to-one-uni/src/com/shri/hibernate/demo/entity/Instructor.java
@@ -11,7 +11,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "instructor")
+@Table()
 public class Instructor {
 	
 	// annotate the class as an entity and map it to the db table
